fe97b052a07a856969874b87641bc631979f988c
Richard Stöckl <richard.stoeckl@ait.ac.at>
2026-02-27 15:31:09 +0100

    Add basic http handlers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/foo", get(get_foo).post(post_foo))
+        .route("/foo/bar", get(foo_bar));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .expect("Unable to listen for tokio");
+    axum::serve(listener, app)
+        .await
+        .expect("Unable to serve with axum");
 }
+
+async fn root() {}
+async fn get_foo() {}
+async fn post_foo() {}
+async fn foo_bar() {}
